e18fc8e834fcffc0e511b98fe22f879f492a334c
Ephraim Solomon <efisolm22@gmail.com>
2024-02-24 11:35:08 +0300

    style

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,21 +1,26 @@
 // Testimonials.js
 import React from 'react';
 import '../styles/testimony.css' //import custom CSS for the testimonial page
-const Testimonials = () => {
-  return (
-    <section id="testimonials" className="testimonials-section">
-      <div className="container">
-        <h2 className="section-heading">Testimonials</h2>
-        <div className="testimonial">
-          <blockquote>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget turpis justo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-            <cite>- John Doe, CEO</cite>
-          </blockquote>
-        </div>
-        {/* Add more testimonials as needed */}
-      </div>
-    </section>
-  );
-};
 
-export default Testimonials;
+const Testimonials = () => {
+    return (
+      <section id="testimonials" className="testimonials-section">
+        <div className="container">
+          <h2 className="section-heading">Testimonials</h2>
+          <div className="row">
+            <div className="col-md-6">
+              <div className="card">
+                <div className="card-body">
+                  <p className="card-text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eget turpis justo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."</p>
+                  <footer className="blockquote-footer">John Doe, CEO</footer>
+                </div>
+              </div>
+            </div>
+            {/* Add more testimonial cards as needed */}
+          </div>
+        </div>
+      </section>
+    );
+  };
+  
+  export default Testimonials;
